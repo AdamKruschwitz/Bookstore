8790eef8eccc8aa5d78d8c786da39f8be5c9d983
yemo06 <yemoafo@github.com>
2017-11-30 11:27:32 -0500

    Person.h changes

--- Person.h
+++ Person.h
@@ -12,6 +12,37 @@ private:
     std::string number;
     std::string email;
     std::string preference;
+
+public:
+    Person();
+    Person(std::string name,std::string number,std::string email,std::string preference);
+    std::string getName();
+    /**
+    * gets the name of the Person
+    * @returns the name of the person
+    */
+    std::string getNumber();
+    /**
+    * gets the number of the person
+    * @returns the number of the person
+    */
+    std::string getEmail();
+    /**
+    * gets the email of the person
+    * @returns the email of the person
+    */
+    std::string getPreference();
+    /**
+    * gets the preference of the person
+    * @returns either call, text, or email, and the corresponding number or email
+    */
+
+    void setName(std::string Name);
+    void setNumber(std::string Number);
+    void setEmail(std::string Email);
+    void setPreference(std::string Number);
+
+
 };
 
 
